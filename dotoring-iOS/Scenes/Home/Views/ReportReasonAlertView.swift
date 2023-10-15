d9e987c88fb92ad6d4d9daadcd893cd19a89e132
rayleigh <rayleigh190@gmail.com>
2023-10-15 22:11:41 +0900

    feat: user 상세페이지 신고 alert에 필요한 label&button 선언

--- dotoring-iOS/Scenes/Home/Views/ReportReasonAlertView.swift
+++ dotoring-iOS/Scenes/Home/Views/ReportReasonAlertView.swift
@@ -23,6 +23,13 @@ class ReportReasonAlertView: UIView {
         return view
     }()
     
+    private lazy var statusBarLabel: NanumLabel = {
+        let label = NanumLabel(weightType: .EB, size: 17)
+        label.text = "신고 사유"
+        
+        return label
+    }()
+    
     lazy var contentLabel: NanumLabel = {
         let label = NanumLabel(weightType: .R, size: 17)
         label.text = "Content Text"
@@ -31,6 +38,91 @@ class ReportReasonAlertView: UIView {
         return label
     }()
     
+    private var contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        
+        return stackView
+    }()
+    
+    private var reason1Label: NanumLabel = {
+        let label = NanumLabel(weightType: .R, size: 17)
+        label.text = "낚시/도배"
+        
+        return label
+    }()
+    
+    private var reason2Label: NanumLabel = {
+        let label = NanumLabel(weightType: .R, size: 17)
+        label.text = "유출/사칭/사기"
+        
+        return label
+    }()
+    
+    private var reason3Label: NanumLabel = {
+        let label = NanumLabel(weightType: .R, size: 17)
+        label.text = "욕설/비하"
+        
+        return label
+    }()
+    
+    private var reason4Label: NanumLabel = {
+        let label = NanumLabel(weightType: .R, size: 17)
+        label.text = "음란물/불건전한 만남 및 대화"
+        
+        return label
+    }()
+    
+    private lazy var reason1Button: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "checkmark"), for: .normal)
+        button.tintColor = .gray
+        button.setContentHuggingPriority(.defaultLow, for: .horizontal)
+//        button.addTarget(self, action: #selector(agreeConfirmButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var reason2Button: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "checkmark"), for: .normal)
+        button.tintColor = .gray
+        button.setContentHuggingPriority(.defaultLow, for: .horizontal)
+//        button.addTarget(self, action: #selector(agreeConfirmButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var reason3Button: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "checkmark"), for: .normal)
+        button.tintColor = .gray
+        button.setContentHuggingPriority(.defaultLow, for: .horizontal)
+//        button.addTarget(self, action: #selector(agreeConfirmButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var reason4Button: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "checkmark"), for: .normal)
+        button.tintColor = .gray
+        button.setContentHuggingPriority(.defaultLow, for: .horizontal)
+//        button.addTarget(self, action: #selector(agreeConfirmButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var secondaryLabel: NanumLabel = {
+        let label = NanumLabel(weightType: .R, size: 13)
+        label.text = "신고가 접수 후엔 취소가 불가능합니다."
+        label.textColor = .BaseSecondaryEmhasisGray
+        label.textAlignment = .center
+        
+        return label
+    }()
     
     lazy var buttonStackView: UIStackView = {
         let stackView = UIStackView()
