eaee9af3d39f26105c868d7e69bd28d7b7bd4d98
rayleigh <rayleigh190@gmail.com>
2023-12-01 16:48:09 +0900

    change: 인트로 화면 오토레이아웃 수정

--- dotoring-iOS/Scenes/Common/IntroViewController.swift
+++ dotoring-iOS/Scenes/Common/IntroViewController.swift
@@ -38,6 +38,7 @@ class IntroViewController: UIViewController {
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.image = UIImage(named: "GreenDotoriLogoImg")
+        imageView.setContentCompressionResistancePriority(.required, for: .vertical)
 
         return imageView
     }()
@@ -121,6 +122,7 @@ class IntroViewController: UIViewController {
         attributedTitle.addAttribute(.baselineOffset, value: spacing, range: NSRange(location: 0, length: attributedTitle.length))
         
         button.setAttributedTitle(attributedTitle, for: .normal)
+        button.setContentCompressionResistancePriority(.required, for: .vertical)
         
         return button
     }()
@@ -146,7 +148,7 @@ private extension IntroViewController {
         titleLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().inset(151).priority(.low)
-            $0.top.greaterThanOrEqualToSuperview().inset(10).priority(.required)
+            $0.top.greaterThanOrEqualToSuperview().inset(50).priority(.high)
         }
         
         mainLogoImageView.snp.makeConstraints {
@@ -158,16 +160,18 @@ private extension IntroViewController {
             $0.centerX.equalTo(mainLogoImageView)
             $0.leading.equalToSuperview().offset(52)
             $0.top.equalTo(mainLogoImageView.snp.bottom).offset(38.66)
-            $0.height.equalTo(103.0)
+            $0.height.equalTo(103.0).priority(251)
+            $0.height.greaterThanOrEqualTo(80.0).priority(.high)
         }
         
         mentiButton.snp.makeConstraints {
             $0.centerX.equalTo(mentoButton)
             $0.leading.equalTo(mentoButton.snp.leading)
             $0.top.equalTo(mentoButton.snp.bottom).offset(12)
-            $0.height.equalTo(103.0)
-            $0.bottom.equalTo(goLoginButton.snp.top).offset(-100).priority(.high)
-            $0.bottom.greaterThanOrEqualTo(goLoginButton.snp.top).offset(-50).priority(.high)
+            $0.height.equalTo(103.0).priority(251)
+            $0.height.greaterThanOrEqualTo(80.0).priority(.high)
+            $0.bottom.equalTo(goLoginButton.snp.top).offset(-100).priority(751)
+            $0.bottom.greaterThanOrEqualTo(goLoginButton.snp.top).offset(-10).priority(751)
         }
         
         mentoBtnLabel.snp.makeConstraints {
@@ -180,7 +184,8 @@ private extension IntroViewController {
         
         goLoginButton.snp.makeConstraints {
             $0.centerX.equalTo(mentiButton)
-            $0.bottom.equalToSuperview().inset(58)
+            $0.bottom.equalToSuperview().inset(58).priority(.high)
+            $0.bottom.greaterThanOrEqualToSuperview().inset(10).priority(.high)
         }
         
     }
