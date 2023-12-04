1c76b9fc3275df2d0690e3a27b6425bc927fd738
rayleigh <rayleigh190@gmail.com>
2023-12-04 22:13:02 +0900

    change: 멘티 회원가입1 화면 오토레이아웃 수정

--- dotoring-iOS/Scenes/Signup/MenteeSignup1ViewController.swift
+++ dotoring-iOS/Scenes/Signup/MenteeSignup1ViewController.swift
@@ -9,16 +9,8 @@ import UIKit
 
 class MenteeSignup1ViewController: UIViewController {
     
-    private lazy var navTitleLabel: NanumLabel = {
-        let label = NanumLabel(weightType: .R, size: 14)
-        let text = "멘티로 회원가입"
-        label.text = text
-        let attributedStr = NSMutableAttributedString(string: text)
-        attributedStr.addAttribute(.foregroundColor, value: UIColor.BaseNavy!, range: (text as NSString).range(of: "멘티"))
-        label.attributedText = attributedStr
-        
-        return label
-    }()
+    // 뷰 전체 높이 길이
+    let screenHeight = UIScreen.main.bounds.size.height
     
     private lazy var stepBar: SignupStepBar = {
         let bar = SignupStepBar(stepCount: 6, currentStep: 1, style: .mentee)
@@ -27,7 +19,7 @@ class MenteeSignup1ViewController: UIViewController {
     }()
     
     private lazy var titleLabel: NanumLabel = {
-        let label = NanumLabel(weightType: .R, size: 20)
+        let label = NanumLabel(weightType: .R, size: 24)
         let text = "Q. 멘티님은 어떤 분인가요?"
         label.text = text
         let attributedStr = NSMutableAttributedString(string: text)
@@ -40,14 +32,14 @@ class MenteeSignup1ViewController: UIViewController {
     private lazy var content1Label: NanumLabel = {
         let label = NanumLabel(weightType: .R, size: 20)
         label.text = "A. 저는"
-        
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         return label
     }()
     
     private lazy var content2Label: NanumLabel = {
         let label = NanumLabel(weightType: .R, size: 20)
         label.text = "에 다니는"
-        
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         return label
     }()
     
@@ -74,7 +66,7 @@ class MenteeSignup1ViewController: UIViewController {
     
     private lazy var content6Label: NanumLabel = {
         let label = NanumLabel(weightType: .R, size: 20)
-        label.text = "을/를 희망합니다."
+        label.text = "의 멘토를 만나길 희망합니다."
         
         return label
     }()
@@ -90,7 +82,7 @@ class MenteeSignup1ViewController: UIViewController {
             NSAttributedString.Key.paragraphStyle: paragraphStyle
         ])
         lineTextField.textField.attributedPlaceholder = centeredPlaceholder
-        
+        lineTextField.setContentHuggingPriority(.defaultLow, for: .horizontal)
         return lineTextField
     }()
     
@@ -135,7 +127,7 @@ class MenteeSignup1ViewController: UIViewController {
         return button
     }()
     
-    private lazy var jobTextField: LineTextField = {
+    private lazy var fieldTextField: LineTextField = {
         let lineTextField = LineTextField()
         lineTextField.textField.textAlignment = .center
         lineTextField.isUserInteractionEnabled = false
@@ -143,7 +135,7 @@ class MenteeSignup1ViewController: UIViewController {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = .center
 
-        let centeredPlaceholder = NSAttributedString(string: "직무 분야", attributes: [
+        let centeredPlaceholder = NSAttributedString(string: "멘토링 분야", attributes: [
             NSAttributedString.Key.foregroundColor: UIColor.gray,
             NSAttributedString.Key.paragraphStyle: paragraphStyle
         ])
@@ -152,7 +144,7 @@ class MenteeSignup1ViewController: UIViewController {
         return lineTextField
     }()
     
-    private lazy var jobTextFieldButton: UIButton = {
+    private lazy var fieldTextFieldButton: UIButton = {
         let button = UIButton()
         button.addTarget(self, action: #selector(selectTextFieldTapped), for: .touchUpInside)
                
@@ -170,10 +162,10 @@ class MenteeSignup1ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.topItem?.title = ""
-        navigationController?.navigationBar.tintColor = .black
+        
         self.hideKeyboardWhenTappedAround()
         setupSubViews()
+        setupUI()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -190,65 +182,88 @@ class MenteeSignup1ViewController: UIViewController {
 
 private extension MenteeSignup1ViewController {
     
+    func setupUI() {
+        self.navigationController?.navigationBar.tintColor = .BaseGray700
+        self.navigationController?.navigationBar.topItem?.title = ""
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "멘티로 회원가입"
+        titleLabel.textColor = UIColor.label // 전체 글씨 색상
+        titleLabel.font = .nanumSquare(style: .NanumSquareOTFR, size: 15)
+        titleLabel.sizeToFit()
+
+        let mentorRange = (titleLabel.text! as NSString).range(of: "멘티")
+        let attributedString = NSMutableAttributedString(string: titleLabel.text!)
+        attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.BaseNavy!, range: mentorRange)
+
+        titleLabel.attributedText = attributedString
+
+        self.navigationItem.titleView = titleLabel
+    
+    }
+    
     func setupSubViews() {
         
-        [navTitleLabel, stepBar, titleLabel, nextButton, departmentTextFieldButton, jobTextFieldButton].forEach {view.addSubview($0)}
+        [stepBar, titleLabel, nextButton, departmentTextFieldButton, fieldTextFieldButton].forEach {view.addSubview($0)}
         
         [content1Label, content2Label, content3Label, content4Label, content5Label, content6Label].forEach {view.addSubview($0)}
         
-        [schoolTextField, gradeTextField, departmentTextField, jobTextField].forEach {view.addSubview($0)}
-        
-        navTitleLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(38)
-            $0.top.equalToSuperview().offset(104)
-        }
+        [schoolTextField, gradeTextField, departmentTextField, fieldTextField].forEach {view.addSubview($0)}
         
         stepBar.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(38)
-            $0.top.equalTo(navTitleLabel.snp.bottom).offset(87)
+            $0.leading.equalToSuperview().offset(17)
+            if screenHeight <= 568 {
+                $0.top.equalToSuperview().inset(70)
+            } else {
+                $0.top.equalToSuperview().offset(147).priority(.low)
+                $0.top.greaterThanOrEqualToSuperview().inset(30).priority(.required)
+            }
         }
         
         titleLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(38)
+            $0.leading.equalTo(stepBar.snp.leading)
             $0.top.equalTo(stepBar.snp.bottom).offset(25)
         }
         
         content1Label.snp.makeConstraints {
             $0.leading.equalTo(titleLabel.snp.leading)
-            $0.top.equalTo(titleLabel.snp.bottom).offset(66)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(65)
         }
         
         schoolTextField.snp.makeConstraints {
             $0.centerY.equalTo(content1Label)
             $0.leading.equalTo(content1Label.snp.trailing).offset(17)
-            $0.width.equalTo(100)
+            $0.width.greaterThanOrEqualTo(80)
         }
         
         content2Label.snp.makeConstraints {
             $0.centerY.equalTo(schoolTextField)
             $0.leading.equalTo(schoolTextField.snp.trailing).offset(17)
+            $0.trailing.equalToSuperview().inset(58).priority(.high)
+            $0.trailing.greaterThanOrEqualToSuperview().inset(16).priority(.high)
         }
         
         gradeTextField.snp.makeConstraints {
             $0.centerX.equalTo(schoolTextField)
             $0.top.equalTo(schoolTextField.snp.bottom).offset(20)
-            $0.width.equalTo(100)
+            $0.leading.equalTo(schoolTextField.snp.leading)
         }
         
         content3Label.snp.makeConstraints {
             $0.centerY.equalTo(gradeTextField)
-            $0.leading.equalTo(gradeTextField.snp.trailing).offset(17)
+            $0.width.equalTo(91)
+            $0.trailing.greaterThanOrEqualToSuperview().inset(41)
         }
         
         departmentTextField.snp.makeConstraints {
             $0.centerX.equalTo(gradeTextField)
             $0.top.equalTo(gradeTextField.snp.bottom).offset(20)
-            $0.width.equalTo(100)
+            $0.leading.equalTo(gradeTextField.snp.leading)
         }
         
         content4Label.snp.makeConstraints {
             $0.centerY.equalTo(departmentTextField)
-            $0.leading.equalTo(departmentTextField.snp.trailing).offset(17)
+            $0.leading.equalTo(content3Label.snp.leading)
         }
         
         content5Label.snp.makeConstraints {
@@ -256,22 +271,23 @@ private extension MenteeSignup1ViewController {
             $0.top.equalTo(departmentTextField.snp.bottom).offset(26)
         }
         
-        jobTextField.snp.makeConstraints {
+        fieldTextField.snp.makeConstraints {
             $0.centerX.equalTo(departmentTextField)
-            $0.top.equalTo(departmentTextField.snp.bottom).offset(20)
-            $0.width.equalTo(100)
+            $0.top.equalTo(departmentTextField.snp.bottom).offset(15)
+            $0.leading.equalTo(departmentTextField.snp.leading)
         }
         
         content6Label.snp.makeConstraints {
             $0.trailing.equalTo(content4Label.snp.trailing)
-            $0.top.equalTo(jobTextField.snp.bottom).offset(20)
+            $0.top.equalTo(fieldTextField.snp.bottom).offset(20)
         }
         
         nextButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.leading.equalToSuperview().offset(38)
-            $0.top.equalTo(content6Label.snp.bottom).offset(70)
-            $0.height.equalTo(45)
+            $0.leading.equalToSuperview().offset(16)
+            $0.top.equalTo(content6Label.snp.bottom).offset(55)
+            $0.height.equalTo(48)
+            $0.bottom.lessThanOrEqualToSuperview().inset(20).priority(.required)
         }
         
         departmentTextFieldButton.snp.makeConstraints {
@@ -279,9 +295,9 @@ private extension MenteeSignup1ViewController {
             $0.centerX.centerY.equalTo(departmentTextField)
         }
         
-        jobTextFieldButton.snp.makeConstraints {
-            $0.width.height.equalTo(jobTextField)
-            $0.centerX.centerY.equalTo(jobTextField)
+        fieldTextFieldButton.snp.makeConstraints {
+            $0.width.height.equalTo(fieldTextField)
+            $0.centerX.centerY.equalTo(fieldTextField)
         }
         
     }
@@ -291,9 +307,9 @@ extension MenteeSignup1ViewController: SelectViewControllerDelegate {
     
     @objc private func selectTextFieldTapped(sender: UIButton) {
         let vc = SelectViewController()
-        if sender == jobTextFieldButton {
+        if sender == fieldTextFieldButton {
             vc.selectViewControllerDelegate = self
-            vc.titleText = "희망 직무 선택"
+            vc.titleText = "멘토링 분야 선택"
             vc.style = .mentee
         } else if sender == departmentTextFieldButton {
             vc.selectViewControllerDelegate = self
@@ -325,8 +341,8 @@ extension MenteeSignup1ViewController: SelectViewControllerDelegate {
             }
             selectedElementString = String(selectedElementString.dropLast(2))
             
-            if sender == jobTextFieldButton {
-                jobTextField.textField.text = selectedElementString
+            if sender == fieldTextFieldButton {
+                fieldTextField.textField.text = selectedElementString
             } else {
                 departmentTextField.textField.text = selectedElementString
             }
