1e46d85f16dbe063549e184224cf785a2c6d68e0
rayleigh <rayleigh190@gmail.com>
2023-12-07 20:29:54 +0900

    change: 회원가입5 화면 오토레이아웃 수정

--- dotoring-iOS/Scenes/Signup/Signup5ViewController.swift
+++ dotoring-iOS/Scenes/Signup/Signup5ViewController.swift
@@ -11,11 +11,20 @@ class Signup5ViewController: UIViewController {
 
     var signup5View: Signup5View!
     
+    let uiStyle: UIStyle = {
+        if UserDefaults.standard.string(forKey: "UIStyle") == "mento" {
+            return UIStyle.mento
+        } else {
+            return UIStyle.mentee
+        }
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.topItem?.title = ""
         self.hideKeyboardWhenTappedAround()
+        setupNavigationBar()
     }
     
     override func loadView() {
@@ -44,6 +53,38 @@ class Signup5ViewController: UIViewController {
     func nextButtonTapped() {
         let vc = Signup6ViewController()
         navigationController?.pushViewController(vc, animated: false)
+    }
+    
+    func setupNavigationBar() {
+        
+        self.navigationController?.navigationBar.tintColor = .BaseGray700
+        self.navigationController?.navigationBar.topItem?.title = ""
+        
+        let titleLabel = UILabel()
+        var attrRangeText = ""
+        var attrStrColor = UIColor.label
+
+        titleLabel.textColor = UIColor.label // 전체 글씨 색상
+        titleLabel.font = .nanumSquare(style: .NanumSquareOTFR, size: 15)
+        titleLabel.sizeToFit()
+
+        if uiStyle == .mentee {
+            titleLabel.text = "멘티로 회원가입"
+            attrRangeText = "멘티"
+            attrStrColor = .BaseNavy!
+        } else {
+            titleLabel.text = "멘토로 회원가입"
+            attrRangeText = "멘토"
+            attrStrColor = .BaseGreen!
+        }
+        
+        let attributedString = NSMutableAttributedString(string: titleLabel.text!)
+        
+        attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: attrStrColor, range: (titleLabel.text! as NSString).range(of: attrRangeText))
+
+        titleLabel.attributedText = attributedString
+
+        self.navigationItem.titleView = titleLabel
     }
     
 }
