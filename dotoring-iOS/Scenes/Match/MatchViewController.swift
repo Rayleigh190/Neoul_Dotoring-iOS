2deb18c15612e4218c933b9e2b1517402a616158
rayleigh <rayleigh190@gmail.com>
2024-01-05 16:31:33 +0900

    feat: 매칭화면 필터 버튼 구현

--- dotoring-iOS/Scenes/Match/MatchViewController.swift
+++ dotoring-iOS/Scenes/Match/MatchViewController.swift
@@ -9,17 +9,37 @@ import UIKit
 import SnapKit
 
 class MatchViewController: UIViewController {
+    let businessNameList = ["교학상장", "캡스톤디자인 지원사업", "기타"]
+    let pjtGoalList = ["공모전", "대외활동", "학업", "학교생활", "기타"]
+    var selectedBusinessNameElements: [Int] = []
+    var selectedPjtGoalElements: [Int] = []
+    
+    private let uiStyle: UIStyle = {
+        if UserDefaults.standard.string(forKey: "UIStyle") == "mento" {
+            return UIStyle.mento
+        } else {
+            return UIStyle.mentee
+        }
+    }()
+    
+    private lazy var baseColor: UIColor = {
+        if uiStyle == .mento {
+            return UIColor.BaseGreen!
+        } else {
+            return UIColor.BaseNavy!
+        }
+    }()
+    
     private let scrollView = UIScrollView()
     private let contentView = UIView()
-
+    let bannerSectionView = BannerSectionView(frame: .zero)
+    let searchSectionView = SearchSectionView(frame: .zero)
+    
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .equalSpacing
         stackView.spacing = 0.0
-
-        let bannerSectionView = BannerSectionView(frame: .zero)
-        let searchSectionView = SearchSectionView(frame: .zero)
 
         let spacingView = UIView()
         spacingView.backgroundColor = .systemBackground
@@ -42,6 +62,7 @@ class MatchViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1)
         setupNavigationController()
+        setupAddTarget()
         setupLayout()
     }
 
@@ -52,6 +73,11 @@ private extension MatchViewController {
         navigationItem.title = "매칭"
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func setupAddTarget() {
+        searchSectionView.businessNameFilterButton.addTarget(self, action: #selector(searchFilterButtonTapped), for: .touchUpInside)
+        searchSectionView.pjtGoalFilterButton.addTarget(self, action: #selector(searchFilterButtonTapped), for: .touchUpInside)
     }
 
     func setupLayout() {
@@ -74,4 +100,60 @@ private extension MatchViewController {
             $0.edges.equalToSuperview()
         }
     }
+}
+
+extension MatchViewController: SelectViewControllerDelegate {
+    
+    @objc private func searchFilterButtonTapped(sender: UIButton) {
+        let vc = SelectViewController()
+        if sender == searchSectionView.businessNameFilterButton {
+            vc.selectViewControllerDelegate = self
+            vc.titleText = "지원사업명"
+            vc.style = .mento
+            vc.elements = businessNameList
+            vc.previousSelectedElements = selectedBusinessNameElements
+        } else if sender == searchSectionView.pjtGoalFilterButton {
+            vc.selectViewControllerDelegate = self
+            vc.titleText = "프로젝트 목표"
+            vc.style = .mento
+            vc.elements = pjtGoalList
+            vc.previousSelectedElements = selectedPjtGoalElements
+        } else {
+            vc.selectViewControllerDelegate = self
+            vc.titleText = "필터"
+        }
+        vc.sender = sender
+        
+        if let sheet = vc.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            sheet.preferredCornerRadius = 30
+            sheet.prefersGrabberVisible = true
+       }
+       present(vc, animated: true)
+    }
+    
+    // 지원사업명, 프로젝트 목표 필터 뷰컨트롤러 화면이 사라질 때 선택한 데이터를 받음
+    func didSelectViewControllerDismiss(elements: [String], selectedElements: [Int], sender: UIButton) {
+        // 선택한 데이터가 0개 이상일 때만 데이터 저장 및 뷰 수정
+        if selectedElements.count > 0 {
+            if sender == searchSectionView.businessNameFilterButton {
+                // 이 뷰컨트롤러에 선택한 데이터 저장하는 코드
+                self.selectedBusinessNameElements = selectedElements
+                searchSectionView.businessNameFilterButton.backgroundColor = baseColor
+            } else {
+                self.selectedPjtGoalElements = selectedElements
+                searchSectionView.pjtGoalFilterButton.backgroundColor = baseColor
+            }
+        } else {
+            if sender == searchSectionView.businessNameFilterButton {
+                selectedBusinessNameElements = []
+                searchSectionView.businessNameFilterButton.backgroundColor = .BaseGray700
+            } else {
+                selectedPjtGoalElements = []
+                searchSectionView.pjtGoalFilterButton.backgroundColor = .BaseGray700
+            }
+        }
+        
+    }
+    
 }
